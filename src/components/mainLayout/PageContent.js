80e4b644d5fead0651a8e5fdbf6bf7a85f929e8d
Ahmet Salih CINKAYA <acinkaya@netas.lab.nortel.com>
2018-09-23 16:59:27 +0300

    Add execute command

--- src/components/mainLayout/PageContent.js
+++ src/components/mainLayout/PageContent.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import { Layout, Breadcrumb} from "antd";
+import React from "react";
+import { Layout, Breadcrumb } from "antd";
 
 const { Header, Content, Footer } = Layout;
+
+function executeCommand() {
+  var exec = window.require("child_process").exec;
+  exec("docker ps", function(error, stdout, stderr) {
+    console.log("stdout: " + stdout);
+    console.log("stderr: " + stderr);
+    if (error !== null) {
+      console.log("exec error: " + error);
+    }
+  });
+}
 
 export default class PageContent extends React.Component {
   render() {
@@ -15,6 +26,7 @@ export default class PageContent extends React.Component {
           </Breadcrumb>
           <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
             Bill is a cat.
+            {executeCommand()}
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
